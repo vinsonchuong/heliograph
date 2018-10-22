b84de342c06b58b256a9ec7b737a481a20cf5fcc
Vinson Chuong <vinsonchuong@gmail.com>
2018-10-21 19:04:48 -0700

    feat(modules): Support Node.js experimental modules

---
+++ sources/index.mjs
@@ -0,0 +1,4 @@
+/* @flow */
+export { default as fromQueue } from './from-queue'
+export { default as fromEventEmitter } from './from-event-emitter'
+export { default as fromStream } from './from-stream'
