503350cab8dc03ef5c7e62a3b6335cfd98e40bf3
Vinson Chuong <vinsonchuong@gmail.com>
2018-11-11 12:11:26 -0800

    feat(fromEventEmitter): Allow listening for ending and error events

--- sources/from-event-emitter/index.js
+++ sources/from-event-emitter/index.js
@@ -4,11 +4,27 @@ import { fromQueue } from 'heliograph'
 
 export default function<Item>(
   eventEmitter: EventEmitter,
-  eventName: string
+  eventName: string,
+  endEventName: ?string,
+  errorEventName: ?string
 ): AsyncIterator<Item> {
   const queue = fromQueue<Item>()
+
   eventEmitter.on(eventName, message => {
     queue.push(message)
   })
+
+  if (endEventName) {
+    eventEmitter.once(endEventName, () => {
+      queue.end()
+    })
+  }
+
+  if (errorEventName) {
+    eventEmitter.once(errorEventName, error => {
+      queue.pushError(error)
+    })
+  }
+
   return queue
 }
--- sources/from-event-emitter/index.test.js
+++ sources/from-event-emitter/index.test.js
@@ -15,3 +15,30 @@ test('queueing up events', async t => {
   t.deepEqual(await iterator.next(), { done: false, value: 2 })
   t.deepEqual(await iterator.next(), { done: false, value: 3 })
 })
+
+test('listening for an ending event', async t => {
+  const eventEmitter = new EventEmitter()
+  const iterator = fromEventEmitter<number>(eventEmitter, 'message', 'end')
+
+  eventEmitter.emit('message', 1)
+  eventEmitter.emit('end')
+
+  t.deepEqual(await iterator.next(), { done: false, value: 1 })
+  t.deepEqual(await iterator.next(), { done: true })
+})
+
+test('listening for an error event', async t => {
+  const eventEmitter = new EventEmitter()
+  const iterator = fromEventEmitter<number>(
+    eventEmitter,
+    'message',
+    null,
+    'error'
+  )
+
+  eventEmitter.emit('message', 1)
+  eventEmitter.emit('error', new Error('Something went wrong'))
+
+  t.deepEqual(await iterator.next(), { done: false, value: 1 })
+  await t.throwsAsync(iterator.next())
+})
