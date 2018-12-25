8c7cc3713746b778196ff5826feea8e39a9e01cd
Vinson Chuong <vinsonchuong@gmail.com>
2018-12-24 21:20:46 -0800

    refactor(fromClock): Rename private function name to be consistent

--- sources/from-clock/index.js
+++ sources/from-clock/index.js
@@ -3,10 +3,12 @@ import { promisify } from 'util'
 
 const sleep = promisify(setTimeout)
 
-export default async function* clock(interval: number): AsyncIterator<number> {
+export default async function* fromClock(
+  interval: number
+): AsyncIterator<number> {
   const nextTick = (Math.floor(Date.now() / interval) + 1) * interval
   await sleep(nextTick - Date.now())
   yield nextTick
 
-  yield* clock(interval)
+  yield* fromClock(interval)
 }
